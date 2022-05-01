51e811911ff09481fc6974c3870905a35c257d6e
HvNelric <37754127+HvNelric@users.noreply.github.com>
2022-05-01 20:08:44 +0200

    App 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.scss';
+import Admin from './components/admin/Admin';
+import Inscription from './components/connexions/Inscription';
+import Login from './components/connexions/Login';
+import { MyContext } from './components/context/MyContext';
+import Landing from './components/landing/Landing';
+import Navbar from './components/navbar/Navbar';
+import { auth } from './firebase/Firebase';
+import './fonts/PlayfairDisplay-Black.ttf'
+import './fonts/SourceSansPro-Regular.ttf'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [userState, setLog] = useState({
+        isLog: false,
+        info: {}
+    })
+
+    useEffect(() => {
+        const authCange = onAuthStateChanged(auth, (user) => {
+            console.log('HOme')
+            if (user) {
+                setLog({
+                    ...userState,
+                    isLog: true,
+                    info: user
+                })
+            }
+        });
+        return () => authCange;
+    }, [])
+
+    return (
+        <MyContext.Provider value={{ userState, setLog }}>
+            
+            <Routes>
+                <Route path='/' element={<Landing />} />
+                <Route path='/inscription' element={<Inscription />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/godmode' element={<Admin />} />
+            </Routes>
+            <Navbar />
+        </MyContext.Provider>
+    );
 }
 
 export default App;
